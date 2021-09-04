69e3153dbfe37ec85e40bc03b68efe6c75a2175f
Muhammed Hashir <mohammedhashir607@gmail.com>
2021-09-04 21:47:14 +0530

    repos issues orgs

--- routes/index.js
+++ routes/index.js
@@ -20,6 +20,58 @@ router.get("/generate/:username", async (req, res) => {
       return response;
     };
 
+    //User Repos Function
+    var get_user_repos = async (username, page_number, prev_data) => {
+      var page = page_number ? page_number : 1;
+
+      var url =
+        "https://api.github.com/users/" + username + "/repos?per_page=100";
+      var data = prev_data ? prev_data : [];
+      if (page_number > 1) {
+        url += "&page=" + page_number;
+      }
+
+      response = await request.get(url);
+
+      data = data.concat(response.data);
+
+      if (response.length == 100) {
+        get_user_repos(username, page + 1, data);
+      } else {
+        return data;
+      }
+    };
+
+    //User Issues Functions
+    var get_user_issues = async (username, page_number, prev_data) => {
+      var page = page_number ? page_number : 1,
+        url =
+          "https://api.github.com/search/issues?q=type:pr+is:merged+author:" +
+          username +
+          "&per_page=100";
+      data = prev_data ? prev_data : [];
+
+      if (page_number > 1) {
+        url += "&page=" + page_number;
+      }
+      response = await request.get(url);
+
+      data = data.concat(response.data);
+      if (response.total_count == 100) {
+        get_user_issues(username, callback, page + 1, data);
+      } else {
+        return data;
+      }
+    };
+
+    //user Orgs Function
+    var get_user_orgs = async (username) => {
+      var url = "https://api.github.com/users/" + username + "/orgs";
+      response = await request.get(url);
+
+      return response;
+    };
+
     //------------CALL FUNCTIONS-------------
     var userdata = await get_user(username);
 
