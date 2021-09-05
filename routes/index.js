e3b424734d73f4c0f206c9da033d38400b95674a
Muhammed Hashir <mohammedhashir607@gmail.com>
2021-09-05 09:08:29 +0530

    more features

--- routes/index.js
+++ routes/index.js
@@ -72,7 +72,70 @@ router.get("/generate/:username", async (req, res) => {
       return response;
     };
 
+    //Sort by Popularity
+    function sortByPopularity(a, b) {
+      return b.popularity - a.popularity;
+    }
+
+    //Sort Languages
+    function sortLanguages(languages, limit) {
+      var languageTotal = 0;
+      var sorted_languages = [];
+
+      for (var lang in languages) {
+        if (typeof lang !== "string") {
+          continue;
+        }
+        sorted_languages.push({
+          name: lang,
+          popularity: languages[lang],
+        });
+
+        languageTotal += languages[lang];
+      }
+
+      if (limit) {
+        sorted_languages = sorted_languages.slice(0, limit);
+      }
+
+      return sorted_languages.sort(sortByPopularity);
+    }
+    var get_starred_resume = async (username, page) => {
+      var star = false;
+      var repos = [];
+      var page = page ? page : 1;
+      var url =
+        "https://api.github.com/users/" +
+        username +
+        "/starred?per_page=100&page=" +
+        page;
+
+      str_repos = await request.get(url);
+      console.log(str_repos.data);
+      for (var i in str_repos.data) {
+        repo = str_repos.data[i];
+
+        if ((i = 0)) {
+          console.log(repo);
+        }
+        if (repo.full_name == "hashirpm/github-resume") {
+          console.log(repo);
+          star = true;
+          return star;
+        }
+      }
+
+      if (repos.length == 100) {
+        star = get_starred_resume(username, page + 1);
+      }
+
+      return star;
+    };
+
     //------------CALL FUNCTIONS-------------
+    const isStarred = await get_starred_resume(username); //Is Starred Calling
+    console.log(isStarred); //Got isStarred
+
     var userdata = await get_user(username);
 
     data = userdata.data;
@@ -111,8 +174,99 @@ router.get("/generate/:username", async (req, res) => {
 
     //console.log(user_data);//Got user data
 
+    data = await get_user_repos(username, 1, []);
+
+    var sorted_repos = [],
+      languages = {},
+      popularity;
+
+    for (var i in data) {
+      repo = data[i];
+
+      if (repo.fork !== false) {
+        continue;
+      }
+
+      if (repo.language) {
+        if (repo.language in languages) {
+          languages[repo.language]++;
+        } else {
+          languages[repo.language] = 1;
+        }
+      }
+
+      popularity = repo.watchers + repo.forks;
+      since = new Date(repo.created_at);
+      since = since.getFullYear();
+      sorted_repos.push({
+        position: i,
+        popularity: popularity,
+        info: repo,
+        since: since,
+      });
+    }
+
+    sorted_repos.sort(sortByPopularity);
+    //console.log(sorted_repos) //Got repos
+    var maxLanguages = 9,
+      languages = sortLanguages(languages, maxLanguages);
+    //console.log(languages); //Got languages
+
+    user_issues = await get_user_issues(username, 1, []);
+    var sorted_issues = [],
+      repos = {};
+
+    for (var i in user_issues[0].items) {
+      issue = user_issues[0].items[i];
+
+      if (repos[issue.repository_url] === undefined) {
+        repo_url = issue.html_url.split("/pull")[0];
+
+        repo_name = repo_url.split("/")[4];
+
+        repos[issue.repository_url] = {
+          popularity: 1,
+          repo_url: repo_url,
+          repo_name: repo_name,
+        };
+      } else {
+        repos[issue.repository_url].popularity += 1;
+      }
+    }
+
+    for (var repo in repos) {
+      obj = repos[repo];
+
+      sorted_issues.push({
+        repo: repo,
+        popularity: obj.popularity,
+        repo_url: obj.repo_url,
+        repo_name: obj.repo_name,
+      });
+    }
+
+    sorted_issues.sort(sortByPopularity);
+    //console.log(sorted_issues)// Got issues
+
+    user_orgs = await get_user_orgs(username);
+
+    var sorted_orgs = [];
+    for (var i in user_orgs.data) {
+      org = user_orgs.data[i];
+      if (org.login === undefined) {
+        continue;
+      }
+      sorted_orgs.push({ position: i, info: org });
+    }
+
+    //console.log(sorted_orgs);//Got all orgs
+
+
     //-------------RENDER-----------------
-    res.render("resume", { user: user_data });
+    res.render("resume", { user: user_data ,      repos: sorted_repos,
+      languages: languages,
+      contributions: sorted_issues,
+      orgs: sorted_orgs,});
   } catch (e) {
     console.log(e);
     res.render("error", { message: "An error occured" });
