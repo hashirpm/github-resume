d995b2a39c6c40414e953eec0bff8d4f522355a3
Muhammed Hashir <mohammedhashir607@gmail.com>
2021-09-05 11:18:05 +0530

    print

---
+++ public/javascripts/script.js
@@ -0,0 +1,12 @@
+function printDiv(divName) {
+    var printContents = document.getElementById(divName).innerHTML;
+    var originalContents = document.body.innerHTML;
+
+    document.body.innerHTML = printContents;
+ 
+
+
+    window.print();
+
+    document.body.innerHTML = originalContents;
+}
